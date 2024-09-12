34ba12958199ad1d195541ae448ea45d188b8797
Ratheesh Raghavendra <ratheehanuram@gmail.com>
2024-09-12 18:45:39 +0530

    Inititalized the app

---
+++ src/main/java/com/turf_booking/booking_sapi/BookingSapiApplication.java
@@ -0,0 +1,13 @@
+package com.turf_booking.booking_sapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingSapiApplication.class, args);
+	}
+
+}
